7e6fb94042c95a3b3f95504cad36d170d9308635
epitron <chris@ill-logic.com>
2022-08-31 11:54:43 -0400

    Use 'URI::RFC2396_Parser' to escape filenames properly (initializing it is kinda slow, so the instance is cached in a 'String' class variable)

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -1,6 +1,9 @@
 class String
   def urlencode
-    ERB::Util.url_encode(self).gsub("%2F", "/")
+    #ERB::Util.url_encode(self).gsub("%2F", "/")
+    @@parser ||= URI::RFC2396_Parser.new
+    @@parser.escape(self)
   end
+  alias_method :urlescape, :urlencode
 end
 
